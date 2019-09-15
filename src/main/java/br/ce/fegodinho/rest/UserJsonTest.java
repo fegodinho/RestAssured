8217ab341d0867e8023e6da97497c7d17a2b2dd3
Felipe Godinho <fegodinho@gmail.com>
2019-09-15 15:05:06 -0300

    Aula 12 - Json Primeiro nível

---
+++ src/main/java/br/ce/fegodinho/rest/UserJsonTest.java
@@ -0,0 +1,47 @@
+package br.ce.fegodinho.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class UserJsonTest {
+	
+	@Test
+	public void deveVerificarPrimeiroNivel() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/1")
+		.then()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name", containsString("Silva"))
+			.body("age", greaterThan(18))			
+			;
+	}
+	
+	@Test
+	public void deveVerificarPrimeiroNivelOutrasFormas() {
+		Response response = RestAssured.request(Method.GET,"http://restapi.wcaquino.me/users/1");
+		
+		//Path
+		Assert.assertEquals(1, response.path("id"));
+		Assert.assertEquals(1, response.path("%s","id"));
+		
+		//JsonPath
+		JsonPath jpath = new JsonPath(response.asString());
+		Assert.assertEquals(1, jpath.getInt("id"));
+		
+		//from
+		int id = JsonPath.from(response.asString()).getInt("id");
+		Assert.assertEquals(1, id);
+	}
+
+}
