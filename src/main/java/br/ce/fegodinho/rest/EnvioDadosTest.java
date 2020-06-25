4649fa7febc22ae319e794e5a8b3e866d0c47764
Felipe Godinho <fegodinho@gmail.com>
2020-06-25 20:13:18 -0300

    Aula 37 - Query parametrizavel

--- src/main/java/br/ce/fegodinho/rest/EnvioDadosTest.java
+++ src/main/java/br/ce/fegodinho/rest/EnvioDadosTest.java
@@ -2,6 +2,8 @@ package br.ce.fegodinho.rest;
 
 import static io.restassured.RestAssured.given;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
@@ -18,6 +20,22 @@ public class EnvioDadosTest {
 			.log().all()
 			.statusCode(200)
 			.contentType(ContentType.JSON)
+		;
+	}
+	
+	@Test
+	public void deveEnviarValorViaQueryViaParam() {
+		given()
+			.log().all()
+			.queryParam("format", "xml")
+			.queryParam("outra", "coisa")
+		.when()
+			.get("http://restapi.wcaquino.me/v2/users")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.contentType(ContentType.XML)
+			.contentType(Matchers.containsString("utf-8"))
 		;
 	}
 
