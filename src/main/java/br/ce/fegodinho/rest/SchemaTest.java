7219fca09d82aa3341906b52f28b41ef990693f1
Felipe Godinho <fegodinho@gmail.com>
2020-07-04 20:34:58 -0300

    Aula 44 - Validando XML

---
+++ src/main/java/br/ce/fegodinho/rest/SchemaTest.java
@@ -0,0 +1,38 @@
+package br.ce.fegodinho.rest;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.Test;
+import org.xml.sax.SAXParseException;
+
+import io.restassured.matcher.RestAssuredMatchers;
+
+public class SchemaTest {
+	
+	@Test
+	public void deveValidarSchemaXML() {
+		given()
+			.log().all()
+		.when()
+			.get("http://restapi.wcaquino.me/usersXML/")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
+		;
+	}
+	
+	@Test(expected = SAXParseException.class)//espera que uma exceção seja lançada
+	public void naoDeveValidarSchemaXMLInvalido () {
+		given()
+			.log().all()
+		.when()
+			.get("http://restapi.wcaquino.me/invalidUsersXML/")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body(RestAssuredMatchers.matchesXsdInClasspath("users.xsd"))
+		;
+	}
+
+}
