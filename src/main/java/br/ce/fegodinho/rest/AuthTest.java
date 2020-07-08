ea150c4da1a3ed9d7d9d49f2e170cfb06ef403c7
Felipe Godinho <fegodinho@gmail.com>
2020-07-07 22:56:55 -0300

    Aula 48 - Acessando API com chave

--- src/main/java/br/ce/fegodinho/rest/AuthTest.java
+++ src/main/java/br/ce/fegodinho/rest/AuthTest.java
@@ -19,5 +19,23 @@ public class AuthTest {
 			.body("name", is("Luke Skywalker"))
 		;
 	}
-
+	
+	@Test
+	public void deveObterClima() {
+		given()
+			.log().all()
+			.queryParam("q","Sorocaba,br")
+			.queryParam("appid", "0f9f1519bb1a665fdcdab9f4eb223eb1")
+			.queryParam("units", "metric")
+		.when()
+			.get("http://api.openweathermap.org/data/2.5/weather")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("Sorocaba"))
+			.body("coord.lon", is(-47.46f))
+			.body("main.temp", greaterThan(20))
+		;
+	}
+	
 }
