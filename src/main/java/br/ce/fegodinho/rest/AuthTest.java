67e214aa7c933ee72e376833197759e45b5d6f61
Felipe Godinho <fegodinho@gmail.com>
2020-07-09 20:17:53 -0300

    Aula 50 - JWT

--- src/main/java/br/ce/fegodinho/rest/AuthTest.java
+++ src/main/java/br/ce/fegodinho/rest/AuthTest.java
@@ -3,7 +3,12 @@ package br.ce.fegodinho.rest;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
+
+import io.restassured.http.ContentType;
 
 public class AuthTest {
 	
@@ -41,53 +46,85 @@ public class AuthTest {
 	@Test
 	public void naoDeveAcessarSemSenha() {
 		given()
-		.log().all()
-	.when()
-		.get("http://restapi.wcaquino.me/basicauth")
-	.then()
-		.log().all()
-		.statusCode(401)
+			.log().all()
+		.when()
+			.get("http://restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(401)
 		;
 	}
 	
 	@Test
 	public void deveFazerAutenticacaoBasica() {
 		given()
-		.log().all()
-	.when()
-		.get("http://admin:senha@restapi.wcaquino.me/basicauth")
-	.then()
-		.log().all()
-		.statusCode(200)
-		.body("status", is("logado"))
+			.log().all()
+		.when()
+			.get("http://admin:senha@restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
 		;
 	}
 	
 	@Test
 	public void deveFazerAutenticacaoBasica2() {
 		given()
-		.log().all()
-		.auth().basic("admin", "senha")
-	.when()
-		.get("http://restapi.wcaquino.me/basicauth")
-	.then()
-		.log().all()
-		.statusCode(200)
-		.body("status", is("logado"))
+			.log().all()
+			.auth().basic("admin", "senha")
+		.when()
+			.get("http://restapi.wcaquino.me/basicauth")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
 		;
 	}
 	
 	@Test
 	public void deveFazerAutenticacaoBasicaChallenge() {
 		given()
-		.log().all()
-		.auth().preemptive().basic("admin", "senha")
-	.when()
-		.get("http://restapi.wcaquino.me/basicauth2")
-	.then()
-		.log().all()
-		.statusCode(200)
-		.body("status", is("logado"))
+			.log().all()
+			.auth().preemptive().basic("admin", "senha")
+		.when()
+			.get("http://restapi.wcaquino.me/basicauth2")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("status", is("logado"))
+		;
+	}
+	
+	@Test
+	public void deveFazerAutenticacaoComTokenJWT() {
+		Map<String, String> login = new HashMap<String, String>();
+		login.put("email", "fegodinho@godinho");
+		login.put("senha", "123456");
+		
+		//login api e receber o token
+		String token = given()
+			.log().all()
+			.body(login)
+			.contentType(ContentType.JSON)
+		.when()
+			.post("http://barrigarest.wcaquino.me/signin")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.extract().path("token");
+		;
+		
+		//obter contas
+		given()
+			.log().all()
+			.header("Authorization", "JWT " + token)
+		.when()
+			.get("http://barrigarest.wcaquino.me/contas")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("nome", hasItem("Conta de teste")) //usa-se hasitem por que retorna uma coleçao de contas []
 		;
 	}
 	
